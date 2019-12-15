cc3a619472708ee566ea4c7e9c480bc468911b98
Aden Mengistie <adensm@icloud.com>
2019-12-15 06:08:14 +0000

    Change Resolution

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -27,6 +27,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
+
+  # Process files as they are uploaded:
+  process resize_to_fill: [800, 350]
 
   # Create different versions of your uploaded files:
   # version :thumb do
